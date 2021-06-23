6c50493a94beb9f95905c6e2d963acb60f140246
Nick Hinchliffe <hinchliffen@wit.edu>
2021-06-23 19:30:30 -0400

    Test2

---
+++ Open Up/Open Up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Open Up
+//
+//  Created by Nick Hinchliffw on 6/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Open Up/Open Up/Open_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_UpApp.swift
+//  Open Up
+//
+//  Created by Nick Hinchliffw on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Open_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
