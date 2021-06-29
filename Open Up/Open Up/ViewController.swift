8ae6ee521216b04ed5f26da4454cc90fd91029d4
Nick Hinchliffe <hinchliffen@wit.edu>
2021-06-28 21:20:50 -0400

    Added additional buttons and handlers. Function implementation coming soon.

--- Open Up/Open Up/ViewController.swift
+++ Open Up/Open Up/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var switchBtn: UIButton!
-
+    @IBOutlet weak var wifiBtn: UIButton!
+    @IBOutlet weak var bluetoothBtn: UIButton!
+    @IBOutlet weak var lastActionField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +25,22 @@ class ViewController: UIViewController {
             print("Switching to Locked")
             switchBtn.setTitle("Locked", for: .normal)
             switchBtn.setTitleColor(.green, for: .normal)
+            lastActionField.text = "Locked @"
         }
         else{
             print("Switching to Unlocked")
             switchBtn.setTitle("Unlocked", for: .normal)
             switchBtn.setTitleColor(.red, for: .normal)
+            lastActionField.text = "Unlocked @"
         }
+    }
+    
+    @IBAction func wifiBtnOnClick(_ sender: Any) {
+        print("Wifi Button Clicked")
+    }
+    
+    @IBAction func bluetoothBtnOnClick(_ sender: Any) {
+        print("BT Button Clicked")
     }
     
 }
