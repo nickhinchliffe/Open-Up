16b3d1046ac642f82b72ea497bfcc6a547739098
Nick Hinchliffe <hinchliffen@wit.edu>
2021-07-20 17:34:24 -0400

    Added Alamofire library for easy http requests

--- Open Up/Open Up/ViewController.swift
+++ Open Up/Open Up/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    
     
     @IBOutlet weak var switchBtn: UIButton!
     @IBOutlet weak var wifiBtn: UIButton!
@@ -17,17 +20,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func switchButtonOnClick(_ sender: Any) {
         if switchBtn.currentTitle == "Unlocked" {
+            AF.request("http:/10.0.0.124:5000/").response { response in
+                debugPrint(response)
+            }
             print("Switching to Locked")
             switchBtn.setTitle("Locked", for: .normal)
             switchBtn.setTitleColor(.green, for: .normal)
             lastActionField.text = "Locked @"
         }
         else{
+            AF.request("http:/10.0.0.124:5000/").response { response in
+                debugPrint(response)
+            }
             print("Switching to Unlocked")
             switchBtn.setTitle("Unlocked", for: .normal)
             switchBtn.setTitleColor(.red, for: .normal)
